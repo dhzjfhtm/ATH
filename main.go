67861023ed8b7d9b14e26112170f973c57f8ce29
eyawnpark <niceian7@yonsei.ac.kr>
2022-10-28 20:19:51 +0900

    Order implemented and account balance implemented

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/adshao/go-binance/v2"
 	"github.com/dhzjfhtm/ATH/realtime/api"
 	"github.com/joho/godotenv"
 )
@@ -28,4 +30,16 @@ func main() {
 			fmt.Println(balance.Asset, ":", balance.Free)
 		}
 	}
+
+	order, err := binanceClient.Client.NewCreateOrderService().Symbol("KLAYUSDT").
+		Side(binance.SideTypeBuy).Type(binance.OrderTypeLimit).
+		TimeInForce(binance.TimeInForceTypeGTC).Quantity("40").
+		Price("0.26").Do(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(order)
+
+	// Use Test() instead of Do() for testing.
 }
