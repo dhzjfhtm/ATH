ed3e70b74f722cea84d484c57bfbd3827b6f869c
wonj <jjaa1012@gmail.com>
2022-11-05 23:06:03 +0900

    logic pseudo code

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/dhzjfhtm/ATH/config"
 	"github.com/dhzjfhtm/ATH/realtime/api"
 	"github.com/dhzjfhtm/ATH/record"
-	"github.com/dhzjfhtm/ATH/trade"
+	"github.com/dhzjfhtm/ATH/strategy"
 	"github.com/joho/godotenv"
 )
 
@@ -16,5 +18,33 @@ func main() {
 	}
 	logger := record.NewLogger()
 	binanceFuture := api.NewBinanceFuture(logger)
-	trade.FutureTrade(binanceFuture)
+	//
+	config.SetFutureConfig(binanceFuture, logger)
+
+	startFlag := false
+
+	for {
+		// 시간 체크 9:00
+		now := time.Now()
+		if now.Hour() == 8 && now.Minute() == 55 {
+
+			// 청산
+
+		} else if now.Hour() == 9 && now.Minute() == 0 {
+			// set leverage and isolated
+
+			// preprocessing
+			// range := 전날 high - low
+			// target price = 오늘 시가 + k(0.5) * range
+
+			// stop limit order
+
+		}
+		if startFlag {
+			strategy.Larry(binanceFuture)
+			time.Sleep(1 * time.Second)
+		}
+
+	}
+
 }
--- strategy/larry.go
+++ strategy/larry.go
@@ -1,1 +1,13 @@
 package strategy
+
+import (
+	"github.com/dhzjfhtm/ATH/realtime/api"
+)
+
+func Larry(binanceFuture *api.BinanceFuture) {
+
+}
+
+func Sell(binanceFuture *api.BinanceFuture) {
+
+}
